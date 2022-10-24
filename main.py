6bde7decab8b28fb55c833731f704f5765d71aab
Alerox5000 <110965242+Alerox5000@users.noreply.github.com>
2022-10-24 07:45:22 -0400

    Update main.py

--- main.py
+++ main.py
@@ -242,7 +242,7 @@ def onmessage(update,bot:ObigramClient):
         tl_admin_user = os.environ.get('tl_admin_user')
 
         #set in debug
-        tl_admin_user = 'AlejandroPupo2001'
+        tl_admin_user = 'CHUQUIIII'
 
         jdb = JsonDatabase('database')
         jdb.check_create()
@@ -818,7 +818,7 @@ def main():
     bot_token = os.environ.get('bot_token')
     print('init bot.')
     #set in debug
-    bot_token = '5727919886:AAH2QxuSpGlCbI6JmIrf4_3e7ksGAtBq7-A'
+    bot_token = '5736336758:AAFVay1EimOF8YUBjw9IUI_WmG8COTGqN1k'
     bot = ObigramClient(bot_token)
     bot.onMessage(onmessage)
     bot.onCallbackData('/cancel ',cancel_task)
